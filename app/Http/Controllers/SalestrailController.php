51fe7e43419962662b9d2ef9a89325b25e871eba
Vincent Ng <vinnie5224@gmail.com>
2025-12-10 07:49:12 +0000

    fixed sailstrail issue

--- app/Http/Controllers/SalestrailController.php
+++ app/Http/Controllers/SalestrailController.php
@@ -4,16 +4,29 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Carbon\Carbon;
 
 class SalestrailController extends Controller
 {
     public function getCalls(Request $request)
     {
-        $from = $request->query('from', '2025-06-01T00:00:00.000Z');
-        $to   = $request->query('to',   '2025-12-01T00:00:00.000Z');
+        // Default FROM date
+        $defaultFrom = '2025-06-01T00:00:00.000Z';
 
-        $username = "adb6b54d-3e23-47dc-8b85-f8ea2332c36f";
-        $password = "dwi8Os4TySSY51YHJourNEarRkNqWp7AJGtxeSoWnCKIdLrkwrDFKv45DQvkuuox";
+        // Normalize "from"
+        try {
+            $from = Carbon::parse(
+                $request->query('from', $defaultFrom)
+            )->toJSON(); // <-- FIXED
+        } catch (\Exception $e) {
+            $from = Carbon::parse($defaultFrom)->toJSON(); // fallback
+        }
+
+        // "to" = NOW in correct Z format
+        $to = Carbon::now('UTC')->toJSON(); // <-- FIXED
+
+        $username = "c7a5829c-3b1f-4226-914c-eaea93bd6b1e";
+        $password = "MowRouieNvmJPCkHi3ldZvlcNH2JF1k6Rr19iHm9SgEMRrE6QF9IJAzne5h5eYJp";
 
         $url = "https://standalone-api.salestrail.io/export/calls/json";
 
